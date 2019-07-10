a3fc127979582f21fd7ee4ebda285b3dbbcf64ff
Hyprxz <88142@farnborough.ac.uk>
2019-07-10 17:04:32 +0100

    added app.py

---
+++ app.py
@@ -0,0 +1,64 @@
+# TODO: add api call to return distance of bus from bus stop in meters
+# TODO: add api call to return time remaining for bus to get to the stop
+
+# stop name bus route
+
+from flask import *
+import readingbusesapi as busWrapper
+import testing_readingapi
+
+app = Flask(__name__)
+
+######### client handler for html/css/js ###################################
+@app.route('/<html_file>', methods=['GET'])
+def get_html(html_file): return render_template(html_file)
+
+
+@app.route('/css/<css_file>')
+def get_css(css_file): return render_template("/css/" + css_file)
+
+
+@app.route('/images/<img_file>')
+def get_img(img_file):
+    print("SENDING IMAGEEEEEEEEEEEEEEEEEEE")
+    return send_from_directory('images/', img_file)
+
+
+@app.route('/fonts/<font_file>')
+def get_font(font_file): return render_template("/fonts/" + font_file)
+######################################################################################
+
+
+@app.route('/get_all_stops')
+def get_all_stops():
+    return all_stops
+
+
+
+
+
+
+[[1, 1, 1, 1, 1, 1],
+ [2, 2, 2, 2, 2, 2],
+ [3, 3, 3, 3, 3, 3],
+ [4, 4, 4, 4, 4, 4],
+ [5, 5, 5, 5, 5, 5]]
+
+indexPos *
+
+
+
+
+# just for looking back at.  Won't actually be used.
+@app.route('/login', methods=['POST'])
+def login():
+
+    print("username: " + request.form['username'])
+    print("password: " + request.form['password'])
+
+    return_data = {"username": request.form['username'], "password": request.form['password']}
+    return jsonify(return_data)
+
+
+if __name__ == '__main__':
+    app.run(host= '0.0.0.0')
