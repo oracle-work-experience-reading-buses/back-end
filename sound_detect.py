ed8474506e1af803d00d45977c08e31f41558e15
Sam Boyden <stboyden@stboyden.xyz>
2019-07-11 11:06:42 +0100

    added the sound detection script for the RaspPi

---
+++ sound_detect.py
@@ -0,0 +1,15 @@
+import audioop
+import pyaudio
+
+chunk = 1024
+p = pyaudio.PyAudio()
+
+stream = p.open(format=pyaudio.paInt16,
+                channels=1,
+                rate=44100,
+                input=True,
+                frames_per_buffer=chunk)
+
+data = stream.read(chunk)
+
+rms = audioop.rms(data, 2)
