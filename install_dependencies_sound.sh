8ccedebabae52f281cdfd5ad0ca9c366dcd6b1fb
Samuel Boyden <stboyden@gmail.com>
2019-07-12 11:10:00 +0100

    Updated sound_detect.py to be less complicated.
    Additionally, the install_dependies_sound script has been updated to reflect the changes made in sound_detect.py

--- install_dependencies_sound.sh
+++ install_dependencies_sound.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
-PACKAGES=("libportaudio2" "python3-pyaudio" "python3-numpy")
+PACKAGES=("libportaudio2" "python3-numpy")
+PIP_PACKAGES=("soundfile" "sounddevice") 
 
 for _package in "${PACKAGES[@]}"; do
 	echo "Installing: ${_package}"
 	echo "$(sudo apt-get install ${_package})"
+	echo ""
+done
+
+for _pip_package in "${PIP_PACKAGES[@]}"; do
+	echo "Installing: ${_pip_package}"
+	echo "$(sudo pip3 install ${_pip_package})"
 	echo ""
 done
--- sound_detect.py
+++ sound_detect.py
@@ -1,124 +1,17 @@
-import audioop
-import pyaudio
-import wave
-import numpy as np
+import soundfile as sf
+import sounddevice as sd
+from datetime import datetime as dt
 
-from sys import byteorder
-from array import array
-from struct import pack
+devices = sd.query_devices()
+device = [i for i in range(len(devices)) \
+          if "USB PnP Audio Device" \
+          in devices[i]["name"]][0]
 
-reference_sound_path = "reference_sound.wav"
-output_sound_path = "recorded_sound.wav"
+sd.default.device = [device, 0]
+print(sd.default.device)
 
-_THRESHOLD = 500
-_CHUNK_SIZE = 1024
-_FORMAT = pyaudio.paInt16
-_RATE = 44100
-
-def is_silent(data):
-    return max(data) < _THRESHOLD
-
-def normalize(data):
-    _MAXIMUM = 16384
-    times = float(_MAXIMUM)/max(abs(i) for i in data)
-    r = array("h")
-    
-    for i in data:
-        r.append(int(i*times))
-    # fixed by oliver
-    return r
-
-def trim(data):
-    def _trim(data):
-        sound_started = False
-        r = array("h")
-        
-        data = [data] if not isinstance(data, array) else data
-        
-        for i in data:
-            if not sound_started and abs(i) > _THRESHOLD:
-                sound_started = True
-                r.append(i)
-            elif sound_started:
-                r.append(i)
-        return r
-    
-    data = _trim(data)
-    data.reverse()
-    data = _trim(data)
-    data.reverse()
-    
-    return data
-
-def add_silence(data, secs):
-    
-    r = array("h", [0 for i in range(int(secs*_RATE))])
-    
-    r.extend(data) # line which errors
-    r.extend([0 for i in range(int(secs*_RATE))])
-    return r
-    
-def record():
-    global _RATE
-    
-    p = pyaudio.PyAudio()
-    
-    input_device = [i for i in range(p.get_device_count()) \
-                    if "USB PnP Audio Device" \
-                    in p.get_device_info_by_index(i).get("name")][0]
-                       
-    if input_device == 0: 
-        raise OSError("Could not find USB microphone device")
-
-    _RATE = int(p.get_device_info_by_index(input_device)["defaultSampleRate"])
-
-    # We get the correct mic
-    stream = p.open(format=_FORMAT, channels=1, rate=_RATE, input=True, 
-                    output=True, frames_per_buffer=_CHUNK_SIZE, 
-                    input_device_index=input_device)
-    # valid stream
-    num_silent = 0
-    sound_started = False
-    
-    r = array("h")
-    
-    while True:
-        data = array("h", stream.read(_CHUNK_SIZE, exception_on_overflow=False))
-        if byteorder == "big":
-            data.byteswap()
-        r.extend(data)
-        
-        silent = is_silent(data)
-        
-        if silent and sound_started:
-            num_silent += 1
-        elif not silent and not sound_started:
-            sound_started = True
-            
-        if sound_started and num_silent > 30:
-            break
-            
-    sample_width = p.get_sample_size(_FORMAT)
-    stream.stop_stream()
-    stream.close()
-    p.terminate()
-    
-    r = normalize(r)
-    r = trim(r)
-
-    r = add_silence(r, 0.5)
-    return sample_width, r
-        
-def record_to_file(path):
-    sample_width, data = record()
-    data = pack("<" +   ("h" * len(data)), *data)
-    
-    wf = wave.open(path, "wb")
-    wf.setnchannels(1)
-    wf.setsampwidth(sample_width)
-    wf.setframerate(_RATE)
-    wf.writeframes(data)
-    wf.close()
-        
-if __name__ == "__main__":
-    record_to_file(output_sound_path)
+sample_rate = 44100
+duration = 4
+recording = sd.rec(int(duration * sample_rate), samplerate=sample_rate, channels=1)
+sd.wait()
+sf.write("recorded_sound.wav", recording, sample_rate)
