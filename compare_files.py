2207fe28800fa25eff925ab9ff8c5e8c2216249a
Samuel Boyden <stboyden@gmail.com>
2019-07-16 16:57:51 +0100

    [WIP] Creates two spectograms.
    Save the spectograms to their own images for comparison.

--- compare_files.py
+++ compare_files.py
@@ -1,23 +1,17 @@
-import librosa
 import matplotlib.pyplot as plt
-from dtw import dtw
+from scipy.io import wavfile
 
-from numpy.linalg import norm
+plt.subplot(211)
+ref_sample_rate, ref_samples = wavfile.read("recorded_sound-2.wav")
+ref_power, ref_frequencies, ref_times, ref_img_axis = plt.specgram(ref_samples, Fs=ref_sample_rate)
+plt.xlabel("Time")
+plt.ylabel("Frequency")
 
-y1, sr1 = librosa.load("recorded_sound-2.wav")
-y2, sr2 = librosa.load("test_for_beep.wav")
 
-plt.subplot(1, 2, 1)
-mfcc1 = librosa.feature.mfcc(y1, sr1)
-librosa.display.specshow(mfcc1)
+plt.subplot(212)
+ref_sample_rate, ref_samples = wavfile.read("reference_sound.wav")
+ref_power, ref_frequencies, ref_times, ref_img_axis = plt.specgram(ref_samples, Fs=ref_sample_rate)
+plt.xlabel("Time")
+plt.ylabel("Frequency")
 
-plt.subplot(1, 2, 2)
-mfcc2 = librosa.feature.mfcc(y2, sr2)
-librosa.display.specshow(mfcc2)
-
-dist, cost, acc_cost, path = dtw(mfcc1.T, mfcc2.T, dist=lambda x, y: norm(x-y, ord=1))
-print(f"Normalised distance: {dist}")
-
-plt.imshow(cost.T, origin="lower", cmap=plt.get_cmap("gray"), interpolation="nearest")
-plt.plot(path[0], path[1], "w")
 plt.show()
