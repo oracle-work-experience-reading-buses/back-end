ad7da04275398091ca3a0d770f554019eabf461c
MichiganAgain <32423164+MichiganAgain@users.noreply.github.com>
2019-07-17 13:14:59 +0100

    Update predict_times.py

--- predict_times.py
+++ predict_times.py
@@ -4,6 +4,7 @@ import requests
 import xmltodict
 from datetime import datetime, timedelta
 from sklearn.externals import joblib
+import oci_file_read as ofr
 
 # import modules.readingbusesapi as busWrapper
 #
@@ -11,16 +12,21 @@ from sklearn.externals import joblib
 
 def predict_times(busAPI, stop_name):
     model = joblib.load("model.pkl")
-
-    avg_times = pd.read_csv('avg_time_from_prev_stop.csv', index_col=0)
-
+    print("predict 1")
+    csv_file = ofr.read_csv("avg_time_from_prev_stop.csv")
+    print("predict 2")
+    avg_times = pd.read_csv(csv_file, index_col=0)
+    print("predict 3")
     stops = pd.DataFrame(busAPI.RequestAllStops())
+    print("predict 4")
     stop_code = stops[stops.description == stop_name].location_code.item()
+    print("predict 5")
 
     #get list of buses coming to the stop
     url = "https://rtl2.ods-live.co.uk//api/siri/sm?key=hvOtkiqAwK&location=" + stop_code
     response = requests.get(url)
     stop_predict = xmltodict.parse(response.content)
+    print("predict 5.5")
     df_temp = pd.DataFrame(i['MonitoredVehicleJourney'] for i in stop_predict['Siri']
                                                                  ['ServiceDelivery']['StopMonitoringDelivery'][
                                                                      'MonitoredStopVisit'][:5])
@@ -30,7 +36,7 @@ def predict_times(busAPI, stop_name):
                                                    df_temp.VehicleRef]})
     stop_predict_df = stop_predict_df.join(monitored_call_df)
     stop_predict_df = stop_predict_df[stop_predict_df.ExpectedArrivalTime != 'cancelled']
-
+    print("predict 5.6")
     #get the last stop that each bus passed through and the delay at that stop
     today = datetime.today().strftime('%Y-%m-%d')
     track_history = [busAPI.Call("TrackingHistory", {"date": today, "vehicle": vehicle})
@@ -39,7 +45,7 @@ def predict_times(busAPI, stop_name):
     # #                                 hist[-1]['LocationName'] if type(hist) is list else "Not known"
     # #                                 for hist in [bus_api.Call("TrackingHistory", {"date": today, "vehicle": vehicle})
     #                                if vehicle != 0 else "Not Known" for vehicle in stop_predict_df.VehicleRef]#]
-
+    print("predict 5.7")
     stop_predict_df['last_stop'] = [hist[-1]['LocationName'] if type(hist) is list else "Not known"
                                     for hist in track_history]
     stop_predict_df['last_stop_delay'] = [(pd.to_datetime(hist[-1]['DepartureTime']) -
@@ -47,9 +53,11 @@ def predict_times(busAPI, stop_name):
                                           if hist is not None else 0
                                           for hist in track_history]
 
+    print("predict 6")
     #get the next stop that each bus will go to
     stop_predict_df['route'] = [pd.DataFrame(busAPI.Call("Route", {"service": bus}))
                                 for bus in stop_predict_df.LineRef]
+    print(stop_predict_df.route)
     stop_predict_df['next_stop'] = [r.location_name[(r[r.location_name == stop_predict_df.last_stop[i]].index + 1) % len(r)].values[0]
                                     if stop_predict_df.last_stop[i] != 'Not known' else 'Not known'
                                     for i, r in enumerate(stop_predict_df.route)]
@@ -63,6 +71,7 @@ def predict_times(busAPI, stop_name):
 
     predicted_delay = pd.to_timedelta(model.predict(features), unit='s')
     aimed_time = pd.to_datetime(stop_predict_df.AimedArrivalTime)
+    print(type((aimed_time+predicted_delay).dt.time))
 
     return (aimed_time+predicted_delay).dt.time
 
